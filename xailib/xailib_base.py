0ee9f62abaef34da754116bf3a495e942175c2c5
riccotti <ric.guidotti@gmail.com>
2020-09-23 14:27:47 +0200

    abstract signatures init

---
+++ xailib/xailib_base.py
@@ -0,0 +1,42 @@
+
+from abc import ABC, abstractmethod
+
+
+class Explainer(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(self, X, y):
+        pass
+
+    @abstractmethod
+    def explain(self, b, y):
+        pass
+
+
+class Explanation(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def getFeaturesImportance(self):
+        pass
+
+    @abstractmethod
+    def getExemplars(self):
+        pass
+
+    @abstractmethod
+    def getCounterExemplars(self):
+        pass
+
+    @abstractmethod
+    def getRules(self):
+        pass
+
+    @abstractmethod
+    def getCounterfactualRules(self):
+        pass
---
+++ xailib/xailib_text.py
@@ -0,0 +1,43 @@
+
+from abc import abstractmethod
+from xailib.xailib_base import Explainer, Explanation
+
+
+class TextExplainer(Explainer):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def fit(self, X, y):
+        pass
+
+    @abstractmethod
+    def explain(self, b, y):
+        pass
+
+
+class TextExplanation(Explanation):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def getFeaturesImportance(self):
+        pass
+
+    @abstractmethod
+    def getExemplars(self):
+        pass
+
+    @abstractmethod
+    def getCounterExemplars(self):
+        pass
+
+    @abstractmethod
+    def getRules(self):
+        pass
+
+    @abstractmethod
+    def getCounterfactualRules(self):
+        pass
