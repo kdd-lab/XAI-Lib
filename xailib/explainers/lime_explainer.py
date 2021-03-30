304a06388c5162b8fc54fc4795f4c1b2bad9e487
francescobodria <francesco.bodria95@gmail.com>
2021-03-30 11:36:39 +0200

    Update LIME with docstring and custom classifier_fn

--- xailib/explainers/lime_explainer.py
+++ xailib/explainers/lime_explainer.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+import numpy as np
 
 from xailib.models.bbox import AbstractBBox
 from xailib.xailib_tabular import TabularExplainer
@@ -6,8 +9,6 @@ from xailib.xailib_image import ImageExplainer
 from externals.lore.datamanager import prepare_dataset
 from lime.lime_tabular import LimeTabularExplainer
 from lime.lime_image import LimeImageExplainer
-
-import matplotlib.pyplot as plt
 
 class LimeXAITabularExplainer(TabularExplainer):
     lime_explainer = None
@@ -33,8 +34,15 @@ class LimeXAITabularExplainer(TabularExplainer):
 
 
 
-    def explain(self, x):
-        exp = self.lime_explainer.explain_instance(x, self.bb.predict_proba)
+    def explain(self, x, classifier_fn=None, num_samples=1000, top_labels=5):
+        if classifier_fn:
+            self.classifier_fn = classifier_fn
+        else:
+            self.classifier_fn = self.bb.predict_proba
+        exp = self.lime_explainer.explain_instance(x, 
+                                                   self.classifier_fn, 
+                                                   um_samples=num_samples, 
+                                                   top_labels=top_labels)
         return exp
 
     def plot_lime_values(self, exp, range_start, range_end):
@@ -57,27 +65,70 @@ class LimeXAIImageExplainer(ImageExplainer):
     lime_explainer = None
 
     def __init__(self, bb: AbstractBBox):
+        """
+        Arguments:
+            bb: black box model
+        """
         super().__init__()
         self.bb = bb
 
-    def wrapper(self, x):
-        return self.bb.predict(x/255)
-
-    def fit(self):
+    def fit(self, verbose=False):
+        """
+        Create the explainer, extra parameters will be set in the explain function
+        """
         self.lime_explainer = LimeImageExplainer(verbose = False)
 
-    def explain(self, x, top_labels=5, num_samples=2000):
-        if x.dtype != 'int':
-            exp = self.lime_explainer.explain_instance((x*255).astype(int),
-                                                       self.wrapper,
-                                                       top_labels=top_labels,
-                                                       hide_color=0,
-                                                       num_samples=num_samples)
+    def explain(self, image, classifier_fn=None, top_labels=5, num_samples=1000):
+        """
+        Return LIME explanation
+        Arguments: 
+            image: query image to explain
+            classifier_fn: [None] function that takes as input an array of images (the LIME neighbourhood) and return an array of (num_images,num_classes)
+                           If None will use black_box.predict function
+            top_labels: For multiclass problems select the best top_labels from the results to produce the explanation
+            num_samples: number of points in the generated neighbourhood
+        """
+        if classifier_fn:
+            self.classifier_fn = classifier_fn
         else:
-            exp = self.lime_explainer.explain_instance(x,
-                                                       self.bb.predict,
-                                                       top_labels=top_labels,
-                                                       hide_color=0,
-                                                       num_samples=num_samples)
-
+            self.classifier_fn = self.bb.predict
+            
+        exp = self.lime_explainer.explain_instance(image,
+                                                   self.classifier_fn,
+                                                   top_labels=top_labels,
+                                                   hide_color=0,
+                                                   num_samples=num_samples)
         return exp
+    
+    def plot_lime_values(self, image, explanation, figsize=(15,5)):
+        """
+        Plot a three figure plot: [query image, heatmap of superpixels, overlap of the twos]
+        Arguments:
+            image: image to explain used in the explain function
+            explanation: explanation returned by the explain function
+            figsize: tuple of figure dimension
+        """
+        
+        F, ax = plt.subplots(1,3,figsize=figsize)
+        ax[0].imshow(image)
+        ax[0].axis('off')
+        ax[0].set_title('Query Image')
+        
+        #plot heatmap
+        ind =  explanation.top_labels[0]
+        dict_heatmap = dict(explanation.local_exp[ind])
+        heatmap = np.vectorize(dict_heatmap.get)(explanation.segments) 
+        ax[1].imshow(heatmap, cmap = 'coolwarm', vmin  = -heatmap.max(), vmax = heatmap.max())
+        ax[1].axis('off')
+        ax[1].set_title('Super Pixel Heatmap Explanation')
+        
+        #plot overlap
+        ax[2].imshow(image)
+        ax[2].imshow(heatmap,alpha=0.5,cmap='coolwarm')
+        ax[2].axis('off')
+        ax[2].set_title('Overlap of Query Image and Heatmap');
+        
+        
+        
+        
+        
