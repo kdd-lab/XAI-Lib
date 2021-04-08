0e5a9e7ccd533fb4cec93a542cc7b19a6f0c3446
francescobodria <francesco.bodria95@gmail.com>
2021-04-08 10:20:40 +0200

    Pytorch Intgrad Integration

--- xailib/explainers/intgrad_explainer.py
+++ xailib/explainers/intgrad_explainer.py
@@ -3,9 +3,10 @@ from xailib.models.bbox import AbstractBBox
 import pandas as pd
 import tensorflow as tf
 import numpy as np
+import torch
 
 
-class IntgradImageExplainer(ImageExplainer):
+class TF_IntgradImageExplainer(ImageExplainer):
     random_state = 0
     bb = None  # The Black Box to be explained
         
@@ -132,3 +133,106 @@ class IntgradImageExplainer(ImageExplainer):
         exp = self.compute_scores(x, target_idx, baseline, m_steps=50, batch_size=32).numpy()
 
         return exp
+
+class PT_IntgradImageExplainer(ImageExplainer):
+    def __init__(self, model):
+        self.bb = model
+        
+    def fit(self):
+        return
+
+    @staticmethod
+    def compute_outputs_and_gradients(inputs, model, target_label_idx, cuda=False):
+        predict_idx = None
+        gradients = []
+        for img in inputs:
+            img.requires_grad = True
+            output = model(img)
+            if target_label_idx is None:
+                target_label_idx = torch.argmax(output, 1).item()
+            index = np.ones((output.size()[0], 1)) * target_label_idx
+            index = torch.tensor(index, dtype=torch.int64)
+            if cuda:
+                index = index.cuda()
+            output = output.gather(1, index)
+            # clear grad
+            model.zero_grad()
+            output.backward()
+            gradient = img.grad.detach().cpu().numpy()[0]
+            gradients.append(gradient)
+        gradients = np.array(gradients)
+        return gradients, target_label_idx
+        
+    @staticmethod
+    def integrated_gradients(image, model, target_label_idx, predict_and_gradients, baseline, steps=50, cuda=False):
+        if baseline is None:
+            baseline = 0 * image 
+        # scale inputs and compute gradients
+        scaled_inputs = [baseline + (float(i) / steps) * (image - baseline) for i in range(0, steps + 1)]
+        grads, _ = predict_and_gradients(scaled_inputs, model, target_label_idx, cuda)
+        avg_grads = np.average(grads[:-1], axis=0)
+        avg_grads = np.transpose(avg_grads, (1, 2, 0))
+        delta_X = (image - baseline).detach().squeeze(0).cpu().numpy()
+        delta_X = np.transpose(delta_X, (1, 2, 0))
+        integrated_grad = delta_X * avg_grads
+        return integrated_grad
+
+    def random_baseline(self, image, model, target_label_idx, predict_and_gradients, steps, num_random_trials, cuda):
+        all_intgrads = []
+        for i in range(num_random_trials):
+            integrated_grad = self.integrated_gradients(self.preprocessing(image), model, target_label_idx, predict_and_gradients,
+                                                        baseline=self.preprocessing(255.0*torch.rand(image.shape)), steps=steps, cuda=cuda)
+            all_intgrads.append(integrated_grad)
+        avg_intgrads = np.average(np.array(all_intgrads), axis=0)
+        return avg_intgrads
+
+    def white_and_black_baseline(self, image, model, target_label_idx, predict_and_gradients, steps, cuda):
+        all_intgrads = []
+
+        integrated_grad = self.integrated_gradients(self.preprocessing(image), model, target_label_idx, predict_and_gradients,
+                                                    baseline=self.preprocessing(255.0*torch.ones(image.shape)), steps=steps, cuda=cuda)
+        all_intgrads.append(integrated_grad)
+        integrated_grad = self.integrated_gradients(self.preprocessing(image), model, target_label_idx, predict_and_gradients, 
+                                                    baseline=self.preprocessing(0.0*torch.ones(image.shape)), steps=steps, cuda=cuda)
+        all_intgrads.append(integrated_grad)
+        avg_intgrads = np.average(np.array(all_intgrads), axis=0)
+        return avg_intgrads
+    
+    def explain(self, image, index_to_explain, baseline, preprocessing, steps=50, cuda=False):
+        """
+        image: image to explain
+        index_to_explain: Which class index of the prediciton to explain, if None the most probable will be selected
+        baseline: baseline to use as reference. Possible choices are ['white', 'black', 'half', 'random']
+            - white will use a white image as baseline
+            - black will use a black one
+            - half will average a white and a black one
+            - random will use random color pixels
+        preprocessing: function that takes as input an image with rgb values 255 and return an image formatted aas the black box needs
+        """
+        self.preprocessing = preprocessing
+
+        # compute the integrated gradients 
+        if baseline == 'white':
+            attributions = self.integrated_gradients(preprocessing(image), self.bb, index_to_explain, self.compute_outputs_and_gradients, 
+                                                     baseline=preprocessing(255.0*torch.ones(image.shape)), steps=steps, cuda=cuda)
+        elif baseline == 'black':
+            attributions = self.integrated_gradients(preprocessing(image), self.bb, index_to_explain, self.compute_outputs_and_gradients, 
+                                                     baseline=preprocessing(0.0*torch.ones(image.shape)), steps=steps, cuda=cuda)
+        elif baseline == 'half':
+            attributions = self.white_and_black_baseline(preprocessing(image), self.bb, index_to_explain, self.compute_outputs_and_gradients, 
+                                                         steps=steps, cuda=cuda)
+        elif baseline == 'random':
+            attributions = self.random_baseline(preprocessing(image), self.bb, index_to_explain, self.compute_outputs_and_gradients, 
+                                                steps=steps, num_random_trials=10, cuda=cuda)
+        else:
+            raise Exception('baseline method not supported')
+        return attributions
+            
+            
+    def visualise(self, attirbutions):
+        img_integrated_gradient_overlay = visualize(attributions, img, clip_above_percentile=99, clip_below_percentile=0, \
+                                                    overlay=True, mask_mode=True)
+        img_integrated_gradient = visualize(attributions, img, clip_above_percentile=99, clip_below_percentile=0, overlay=False)
+        output_img = generate_entrie_images(img, img_gradient, img_gradient_overlay, img_integrated_gradient, \
+                                            img_integrated_gradient_overlay)
+        cv2.imwrite('results/' + args.model_type + '/' + args.img, np.uint8(output_img))
